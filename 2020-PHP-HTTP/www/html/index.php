8ec6326c632e49e68dc6ce9b20f619a712f1a3f1
pochon <pochon.tech.0519@gmail.com>
2020-06-04 19:31:24 +0900

    get json sample

--- 2020-PHP-HTTP/www/html/index.php
+++ 2020-PHP-HTTP/www/html/index.php
@@ -47,50 +47,12 @@ echo $stdInstance->number . PHP_EOL; //42
 $array = json_decode($json, true); // 第２引数をtrueにすると返されるオブジェクトは連想配列形式
 echo $array['foo'] . PHP_EOL; //"bar"
 echo $array['number'] . PHP_EOL; //42
+
 // オブジェクト配列形式のJSONもデコード可能
-$json2 = '
-{
-    "res":
-    {
-        "blogData":
-        [
-            {
-                "id":"0001",
-                "title":"サンプル01",
-                "day":
-                [
-                    {
-                        "year":"2015",
-                        "month":"03",
-                        "_day":"31"
-                    }
-                ],
-                "author":"fantmsite",
-                "tag":
-                [
-                    "ブログ"
-                ],
-                "report":"ブログ内容サンプル01"
-            },
-            {
-                "id":"0002",
-                "title":"サンプル02",
-                "day":
-                [
-                    {
-                        "year":"2015",
-                        "month":"04",
-                        "_day":"01"
-                    }
-                ],
-                "author":"fantmsite",
-                "tag":
-                [
-                    "ブログ"
-                ],
-                "report":"ブログ内容サンプル02"
-            }
-        ]
-    }
-}';
+$url = "http://localhost/sample.json";
+// file_get_contents()はファイルの内容を文字列に読み込む方法として好ましい
+$json2 = file_get_contents($url);
+// 読み取ったJSONデータの文字化け回避
+// ASCII,JIS,UTF-8,EUC-JP,SJIS-WINの順番で自動検出し、UTF8に変換
+$json2 = mb_convert_encoding($json2, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 var_dump(json_decode($json2));

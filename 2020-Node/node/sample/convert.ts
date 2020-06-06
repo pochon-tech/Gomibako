6bdd313e57ec6af29f9ea79fb6e02f351142afc2
pochon <pochon.tech.0519@gmail.com>
2020-06-06 17:18:49 +0900

    glob filesearch

--- 2020-Node/node/sample/convert.ts
+++ 2020-Node/node/sample/convert.ts
@@ -1,13 +1,6 @@
 import * as fs from 'fs'
-
-fs.readFile(__dirname + "/html/index.html",{ encoding: 'utf8' }, (err, data) => {
-    const list: string[]| null = data.match(/(({{)(\s*\$t\()(.*)(\)\s*}}))/g)
-    const list2: string[] = list ? list.map(item => {
-        return item.replace(/^{{\s*/, '').replace(/\$t\(("|')/, '').replace(/("|')\)\s*}}$/, '')
-    }) : []
-    console.log(list2)
-    exportCSV(list2)
-})
+import * as path from 'path'
+import glob from 'glob' // yarn add glob @types/glob
 
 const exportCSV = (arr: string[]) => {
     let formatCSV = ''
@@ -16,3 +9,32 @@ const exportCSV = (arr: string[]) => {
     }
     fs.writeFile(__dirname + 'formList.csv', formatCSV, 'utf8', (err) => {})
 }
+
+const searchFiles = (dirPath: string, extPattern: string) => {
+    const pattern = dirPath+extPattern
+    // glob(pattern, (err: Error | null, files: string[]) => {
+    //     fileList = files.map(file => path.basename(file))
+    //     console.log(fileList)
+    // })
+    try {
+        const files = glob.sync(pattern)
+        console.log(files)
+        return files.length ? files.map(file=> path.basename(file)) : []
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const main = (): void => {
+    searchFiles(__dirname + "/html", "**/*.html")
+    fs.readFile(__dirname + "/html/index.html",{ encoding: 'utf8' }, (err, data) => {
+        const list: string[]| null = data.match(/(({{)(\s*\$t\()(.*)(\)\s*}}))/g)
+        const list2: string[] = list ? list.map(item => {
+            return item.replace(/^{{\s*/, '').replace(/\$t\(("|')/, '').replace(/("|')\)\s*}}$/, '')
+        }) : []
+        console.log(list2)
+        exportCSV(list2)
+    })   
+}
+
+main()

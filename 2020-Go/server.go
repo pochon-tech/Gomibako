60ac7f2b94ca38ba7719d3de260c04f4fd02b5ed
pochon <pochon.tech.0519@gmail.com>
2020-06-14 16:32:04 +0900

    go routing, response

--- 2020-Go/server.go
+++ 2020-Go/server.go
@@ -1,9 +1,17 @@
+// 扱い特殊。main関数を定義することで、エントリポイントとして使用できる
 package main
 
 import (
+	"fmt"
     "net/http"
 )
 
 func main() {
+	// 「/hoge」に対して処理を追加する
+	http.HandleFunc("/hoge", handler)
     http.ListenAndServe(":8003", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
 }

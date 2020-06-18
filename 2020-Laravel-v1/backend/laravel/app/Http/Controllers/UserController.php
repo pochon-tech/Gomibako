22c65ae224d265162853296756a3c57323d892c9
pochon <pochon.tech.0519@gmail.com>
2020-06-18 18:06:45 +0900

    modifired UserController create process

--- 2020-Laravel-v1/backend/laravel/app/Http/Controllers/UserController.php
+++ 2020-Laravel-v1/backend/laravel/app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\RedirectResponse;
 
 class UserController extends Controller
 {
@@ -17,5 +17,32 @@ class UserController extends Controller
     {
         $users = (new UserService())->list();
         return view('user.list', ['users' => $users]);
+    }
+    /**
+     * User登録ページ
+     * 
+     * @return View
+     */
+    public function create(): View
+    {
+        // postした内容を取得 Confirmページを設ける場合もこれを使う
+        // Validation時に入力情報を残したいため、行っている
+        return view('user.create', ['user' => \Session::get('_old_input')]);
+    }
+    /**
+     * User登録処理
+     * 
+     * @return StoreUserPost $request
+     * @return RedirectResponse 
+     */
+    public function store(StoreUserPost $request): RedirectResponse
+    {
+        $user = $request->all();
+        if ((new UserService())->store($user)) {
+            // with(): フラッシュデータ指定のリダイレクト
+            // trans(): 多言語対応。resource/lang/ja/以下で定義する配列のキーを指定
+            return redirect('/user/')->with('success_message', trans('message.success_save'));
+        }
+        return redirect('/user/')->with('error_message', trans('message.failed_save'));
     }
 }
